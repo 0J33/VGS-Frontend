0fcfb2b13d1ad71833decb4bb73f57963a3d61a3
Ojee <omargamal.02.11@gmail.com>
2023-05-10 16:42:09 +0300

    edited cms styling

--- src/components/ResourceCard.jsx
+++ src/components/ResourceCard.jsx
@@ -2,10 +2,10 @@ import "../css/resource-card.css";
 
 export default function ResourceCard({resource_name, resource_type, resource_link}) {
     return (
-        <div className="card" style={{width:"500px",boxSizing:"border-box", padding:"5px 30px", backgroundColor:"#111111"}}>
-            <h1 className="heading-text" style={{color:"white"}}>{resource_name}</h1>
-            <p className="paragraph-text" style={{color:"white"}}>{resource_type}</p>
-            <a className="description-text" style={{color: "#33B4C1", textDecoration:"None"}} href={resource_link}>DOWNLOAD</a>
+        <div className="card" style={{width:"750px",boxSizing:"border-box", padding:"5px 30px", backgroundColor:"#111111"}}>
+            <h1 className="header-text" style={{color:"white", fontSize:"25px", marginTop:"15px", fontWeight:"600"}}>{resource_name}</h1>
+            <p className="paragraph-text" style={{color:"grey"}}>{resource_type}</p>
+            <button className="btn-cms"><a className="description-text" style={{color: "white", textDecoration:"None"}} href={resource_link}>DOWNLOAD</a></button>
             <br></br>
         </div>
     )
