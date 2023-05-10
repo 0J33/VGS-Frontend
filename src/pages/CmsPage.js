8c44d1a679d02766982a2b1acfe3d48a59e0bbbb
Ojee <omargamal.02.11@gmail.com>
2023-05-10 17:27:52 +0300

    added select cms

--- src/pages/CmsPage.js
+++ src/pages/CmsPage.js
@@ -6,17 +6,32 @@ import ResourceCard from "../components/ResourceCard";
 import LoadingSpinner from "../components/LoadingSpinner";
 
 const gdd_url = "https://vgs-production.up.railway.app/api/gdd";
+const gad_url = "https://vgs-production.up.railway.app/api/gad";
+const gsd_url = "https://vgs-production.up.railway.app/api/gsd";
 
 export default function CmsPage() {
     const navigate = useNavigate();
     const [data, setData] = useState([]);
 
     const [isLoading, setIsLoading] = useState(false);
+    const [selectedCommittee, setSelectedCommittee] = useState(0);
 
     async function getGDD() {
         setIsLoading(true);
-      await axios.get(gdd_url)
-        .then(res => {setData(res.data); setIsLoading(false)}); 
+        await axios.get(gdd_url)
+            .then(res => {setData(res.data); setIsLoading(false)}); 
+    }
+
+    async function getGAD() {
+        setIsLoading(true);
+        await axios.get(gad_url)
+            .then(res => {setData(res.data); setIsLoading(false)}); 
+    }
+
+    async function getGSD() {
+        setIsLoading(true);
+        await axios.get(gsd_url)
+            .then(res => {setData(res.data); setIsLoading(false)}); 
     }
 
     function getCookie() {
@@ -36,17 +51,47 @@ export default function CmsPage() {
 
     return (
         <>
+
             {
                 isLoading ? (
                     <div className="body">
                         <LoadingSpinner />
                     </div>
                 ) : (
-                <div className="body">
+                <div className="body" style={{justifyContent:"normal"}}>
+                    <div className="committee-selection-box">
+                        <div className={selectedCommittee === 0 ? "selected-committee-button-wrapper": "committee-button-wrapper"}
+                            onClick={() => {
+                                setSelectedCommittee(0);
+                                getGDD();
+                            }}
+                        >
+                            <p style={{color:"black", fontFamily:"sen", fontWeight:900}}>GDD</p>
+                        </div>
+                        <div className={selectedCommittee === 1 ? "selected-committee-button-wrapper": "committee-button-wrapper"}
+                            onClick={() => {
+                                setSelectedCommittee(1);
+                                getGAD();
+                            }}
+                        >
+                            <p style={{color:"black", fontFamily:"sen", fontWeight:900}}>GAD</p>
+                        </div>
+                        <div className={selectedCommittee === 2 ? "selected-committee-button-wrapper": "committee-button-wrapper"}
+                            onClick={() => {
+                                setSelectedCommittee(2);
+                                getGSD();
+                            }}
+                        >
+                            <p style={{color:"black", fontFamily:"sen", fontWeight:900}}>GSD</p>
+                        </div>
+                    </div><br></br>
                     {
                         data.map((keys, idx) => {
                             return (
-                                <ResourceCard key={idx} resource_name={keys.resource_name} resource_type={keys.resource_type} resource_link={keys.resource_link} />
+
+                                <div>
+                                    <ResourceCard key={idx} resource_name={keys.resource_name} resource_type={keys.resource_type} resource_link={keys.resource_link} />
+                                </div>
                             );
                         })
                     }
